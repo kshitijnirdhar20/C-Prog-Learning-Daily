21e42edbf2f808f65c0d47a011b73e3708f8fb91
KSHITIJ DINESH NIRDHAR <nirdharkshitij2006@gmail.com>
2025-08-04 22:51:01 +0530

    Add files via upload

---
+++ Multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,multiply;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    
+    multiply=a*b;
+    
+    printf("mult = %d\n", multiply);
+    
+}
